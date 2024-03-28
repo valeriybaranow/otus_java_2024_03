c613fcd968977fcd6dff9c3c919b97530a69eb27
baranov.v.a <baranov.v.a@eltel.com>
2024-03-28 05:00:45 +0300

    l-06: выполнить ДЗ аннотации

---
+++ L06-annotations/src/main/java/org/example/TestClass.java
@@ -0,0 +1,14 @@
+package org.example;
+
+
+import org.example.annotations.TestClass;
+
+public class Test {
+
+    @TestClass
+    public void Func1()  {
+        System.out.println("func1");
+    }
+
+
+}
